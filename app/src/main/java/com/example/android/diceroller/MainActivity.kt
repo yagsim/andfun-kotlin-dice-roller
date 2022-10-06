1252ae45b1e71d21fede4bcf26cf4e2ff670480c
yagsim <yagosimofernandez@gmail.com>
2022-10-06 13:30:05 +0200

    Step 02 y toast

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -18,8 +18,12 @@ package com.example.android.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,5 +34,7 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         }
+
     }
+   
 }
