a6cdfd9acc85bbaa8151ca374bf50033a2cf177c
yagsim <yagosimofernandez@gmail.com>
2022-10-06 13:32:07 +0200

    Step 04

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -40,5 +40,13 @@ class MainActivity : AppCompatActivity() {
         val randomInt: Int = Random.nextInt(6) + 1
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+        val drawableResource=when(randomInt){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
     }
 }
